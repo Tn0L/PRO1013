b5cf4b6dacacc33d269ec9df09e28c5e082c244c
Tn Luong <tn0luong@gmail.com>
2019-11-14 15:24:04 +0700

    update banner

--- mvc/index.php
+++ mvc/index.php
@@ -1,20 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx" class="no-js">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="view/img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Karma Shop</title>
+	<!--
+		CSS
+		============================================= -->
+	<link rel="stylesheet" href="view/css/linearicons.css">
+	<link rel="stylesheet" href="view/css/font-awesome.min.css">
+	<link rel="stylesheet" href="view/css/themify-icons.css">
+	<link rel="stylesheet" href="view/css/bootstrap.css">
+	<link rel="stylesheet" href="view/css/owl.carousel.css">
+	<link rel="stylesheet" href="view/css/nice-select.css">
+	<link rel="stylesheet" href="view/css/nouislider.min.css">
+	<link rel="stylesheet" href="view/css/ion.rangeSlider.css" />
+	<link rel="stylesheet" href="view/css/ion.rangeSlider.skinFlat.css" />
+	<link rel="stylesheet" href="view/css/magnific-popup.css">
+	<link rel="stylesheet" href="view/css/main.css">
 </head>
+
 <body>
-<?php
-    include 'view/header.php';
-    include 'view/big-banner.php';
-    include 'view/main-feature.php';
-    include 'view/main-category.php';
-    include 'view/main-product.php';
-    include 'view/main-related-product.php';
-    include 'view/footer.php';
-?>
+
+    <?php
+        include '../mvc/view/header.php';
+        include '../mvc/view/big-banner.php';
+        include '../mvc/view/main.php';
+        include '../mvc/view/footer.php';
+    ?>
+
+    <!-- lib -->
+	<script src="view/js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.view/js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	 crossorigin="anonymous"></script>
+	<script src="view/js/vendor/bootstrap.min.js"></script>
+	<script src="view/js/jquery.ajaxchimp.min.js"></script>
+	<script src="view/js/jquery.nice-select.min.js"></script>
+	<script src="view/js/jquery.sticky.js"></script>
+	<script src="view/js/nouislider.min.js"></script>
+	<script src="view/js/countdown.js"></script>
+	<script src="view/js/jquery.magnific-popup.min.js"></script>
+	<script src="view/js/owl.carousel.min.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="view/js/gmaps.min.js"></script>
+	<script src="view/js/main.js"></script>
 </body>
 </html>
--- mvc/view/footer.php
+++ mvc/view/footer.php
@@ -76,22 +76,3 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 			</div>
 		</div>
 	</footer>
-	<!-- End footer Area -->
-
-	<script src="view/js/vendor/jquery-2.2.4.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.view/js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-	 crossorigin="anonymous"></script>
-	<script src="view/js/vendor/bootstrap.min.js"></script>
-	<script src="view/js/jquery.ajaxchimp.min.js"></script>
-	<script src="view/js/jquery.nice-select.min.js"></script>
-	<script src="view/js/jquery.sticky.js"></script>
-	<script src="view/js/nouislider.min.js"></script>
-	<script src="view/js/countdown.js"></script>
-	<script src="view/js/jquery.magnific-popup.min.js"></script>
-	<script src="view/js/owl.carousel.min.js"></script>
-	<!--gmaps Js-->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
-	<script src="view/js/gmaps.min.js"></script>
-	<script src="view/js/main.js"></script>
-</body>
-</html>
