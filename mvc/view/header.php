a10aaeba62b79ed8eacb1f9fab09cb7ab9156358
nguyen trongnhat <nhatntps10403@fpt.edu.vn>
2019-11-14 20:02:11 +0700

    header menu moi

--- mvc/view/header.php
+++ mvc/view/header.php
@@ -29,58 +29,71 @@
 	<link rel="stylesheet" href="view/css/ion.rangeSlider.css" />
 	<link rel="stylesheet" href="view/css/ion.rangeSlider.skinFlat.css" />
 	<link rel="stylesheet" href="view/css/magnific-popup.css">
-	<link rel="stylesheet" href="view/css/main.css">
+  <link rel="stylesheet" href="view/css/main.css">
+
+  <link rel="stylesheet" href="view/css/header.css">
 </head>
 
 <body>
 
 	<!-- Start Header Area -->
-	<header class="header_area sticky-header">
-		<!-- <div class="main_menu">	 -->
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#">Navbar</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Dropdown
-						</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-					</li>
-					</ul>
-					<form class="form-inline my-2 my-lg-0">
-					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</form>
-				</div>
-				</nav>
-		<!-- </div> -->
-		<div class="search_input" id="search_input_box">
-			<div class="container">
-				<form class="d-flex justify-content-between">
-					<input type="text" class="form-control" id="search_input" placeholder="Search Here">
-					<button type="submit" class="btn"></button>
-					<span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
+	<header>
+		<div class="container"> <!--1180px-->
+			<nav class="navbar navbar-expand-lg navbar-light bg-light main_box">
+				<!-- logo và chữ -->
+				<a class="navbar-brand navbar-light" href="#">
+					<img src="uploaded/logocochu.png" width="90" height="90" class="d-inline-block align-top" alt="">
+					<span class="navbar-brand mb-0 h1 nh-mt">STAR SNEKER</span>
+				</a>
+				<!-- menu -->
+				<form class="form-inline nh-ml">
+					<div class="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link nh-orange" href="#">HOME</a>
+						</li>
+						<li class="nav-item active dropdown nh-mlm">
+							<a class="nav-link" href="#">SHOP</a>
+								<!-- menu xổ -->
+								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a class="dropdown-item" href="#">Shop Category</a>
+								<a class="dropdown-item" href="#">Product Details</a>
+								<a class="dropdown-item" href="#">Product Checkout</a>
+								<a class="dropdown-item" href="#">Shopping Cart</a>
+								</div>
+						</li>
+						<li class="nav-item active dropdown nh-mlm">
+							<a class="nav-link" href="#">BLOGS</a>
+								<!-- menu xổ -->
+								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a class="dropdown-item" href="#">BLOGS</a>
+								<a class="dropdown-item" href="#">BLOGS DETAIL</a>
+								</div>
+						</li>
+						<li class="nav-item active dropdown nh-mlm">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							PAGES
+							</a>
+								<!-- menu xổ -->
+								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a class="dropdown-item" href="#">LOGIN</a>
+								<a class="dropdown-item" href="#">TRACKING</a>
+								<a class="dropdown-item" href="#">ELEMENT</a>
+								</div>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#">CONTACT</a>
+						</li>
+						</ul>
+					</div>
 				</form>
-			</div>
+				<!--Search -->
+				<form class="form-inline nh-ml3">
+					<input class="form-control mr-sm-2" type="search" placeholder="what find.." aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  				</form>
+			</nav>
 		</div>
 	</header>
 	<!-- End Header Area -->
+<!----------------------------------------------------------------------------------------->
